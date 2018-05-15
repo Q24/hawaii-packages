1afb684c11ae51c16e9cbc7a735a36b757a87033
Ruud Voost <ruud.voost@qnh.nl>
2018-05-15 11:28:40 +0200

    ng6

--- tasks/publishAll.js
+++ tasks/publishAll.js
@@ -4,21 +4,21 @@ var exec = require('child_process').exec;
 // Set the glob and options
 var pattern = "packages/**/package.json",
     options = {
-      dot:    true,
-      ignore: '**/node_modules/**/package.json'
+        dot: true,
+        ignore: '**/node_modules/**/package.json'
     };
 
 glob(pattern, options, function (er, files) {
 
-  // Loop through the files
-  files.forEach(function (file) {
-    // Get the dir for the package
-    var packageDir = file.split('/package.json')[0];
+    // Loop through the files
+    files.forEach(function (file) {
+        // Get the dir for the package
+        var packageDir = file.split('/package.json')[0];
 
-    // Execute the build command
-    exec('cd ' + packageDir + ' && npm publish')
-        .stdout
-        .pipe(process.stdout);
-  });
+        // Execute the build command
+        exec('cd ' + packageDir + ' && npm publish')
+            .stdout
+            .pipe(process.stdout);
+    });
 });
 
