3f5133cedf72178c84a805103c866536ca03fb0c
Kevin Riemens <kevin.riemens@qnh.nl>
2018-11-08 15:42:41 +0100

    Revert "feat(Config): Provide config to module in forRoot()"
    This reverts commit 84bf8f7a52a93f57c54b6a6feaa94d28cd3e2415.

--- packages/ngx-oidc-implicit/index.ts
+++ packages/ngx-oidc-implicit/index.ts
@@ -1,12 +1,13 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { OidcService } from './src/oidc.service';
-import { LogService } from './src/services';
-import { OidcConfig } from './src/models';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {OidcService} from './src/oidc.service';
+import {LogService} from './src/services';
+
 
 export * from './src/models';
 export * from './src/constants/authorize.constants';
 export * from './src/oidc.service';
 export * from './src/utils';
+
 
 @NgModule()
 export class OidcModule {
@@ -14,16 +15,27 @@ export class OidcModule {
    * Use this method in your root module to provide the OidcService
    * @returns {ModuleWithProviders}
    */
-  static forRoot(config: OidcConfig): ModuleWithProviders {
+  static forRoot(): ModuleWithProviders {
     return {
       ngModule: OidcModule,
       providers: [
-        {
-          provide: OidcService,
-          useValue: config,
-        },
-        LogService,
-      ],
+        OidcService,
+        LogService
+      ]
+    };
+  }
+
+  /**
+   * Use this method in your other (non root) modules to import stuff you need
+   * @returns {ModuleWithProviders}
+   */
+  static forChild(): ModuleWithProviders {
+    return {
+      ngModule: OidcModule,
+      providers: [
+        OidcService,
+        LogService
+      ]
     };
   }
 }
