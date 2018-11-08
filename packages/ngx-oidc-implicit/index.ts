84bf8f7a52a93f57c54b6a6feaa94d28cd3e2415
Kevin Riemens <kevin.riemens@qnh.nl>
2018-11-08 15:08:18 +0100

    feat(Config): Provide config to module in forRoot()

--- packages/ngx-oidc-implicit/index.ts
+++ packages/ngx-oidc-implicit/index.ts
@@ -1,13 +1,12 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
-import {OidcService} from './src/oidc.service';
-import {LogService} from './src/services';
-
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { OidcService } from './src/oidc.service';
+import { LogService } from './src/services';
+import { OidcConfig } from './src/models';
 
 export * from './src/models';
 export * from './src/constants/authorize.constants';
 export * from './src/oidc.service';
 export * from './src/utils';
-
 
 @NgModule()
 export class OidcModule {
@@ -15,27 +14,16 @@ export class OidcModule {
    * Use this method in your root module to provide the OidcService
    * @returns {ModuleWithProviders}
    */
-  static forRoot(): ModuleWithProviders {
+  static forRoot(config: OidcConfig): ModuleWithProviders {
     return {
       ngModule: OidcModule,
       providers: [
-        OidcService,
-        LogService
-      ]
-    };
-  }
-
-  /**
-   * Use this method in your other (non root) modules to import stuff you need
-   * @returns {ModuleWithProviders}
-   */
-  static forChild(): ModuleWithProviders {
-    return {
-      ngModule: OidcModule,
-      providers: [
-        OidcService,
-        LogService
-      ]
+        {
+          provide: OidcService,
+          useValue: config,
+        },
+        LogService,
+      ],
     };
   }
 }
