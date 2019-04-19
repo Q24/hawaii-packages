88f52820ba88e98a5f7a5cec4ff56d2183db882e
Kevin Riemens <kevin.riemens@qnh.nl>
2019-04-19 16:40:24 +0200

    fix(OIDC Implicit Core): LogUtil fix, and only emitLog when debug = true in config

--- packages/oidc-implicit-core/utils/logUtil.ts
+++ packages/oidc-implicit-core/utils/logUtil.ts
@@ -1,15 +1,9 @@
+import ConfigService from '../services/config.service';
+
 export class LogUtil {
 
-  private static emitLog(logType: 'debug' | 'info' | 'warn' | 'error', msg: string, supportingDetails: any[]) {
-    if (supportingDetails.length > 0) {
-      console[logType](msg, supportingDetails);
-    } else {
-      console[logType](msg);
-    }
-  }
-
   static debug(msg: string, ...supportingDetails: any[]): void {
-    LogUtil.emitLog('debug', msg, supportingDetails);
+    LogUtil.emitLog('log', msg, supportingDetails);
   }
 
   static info(msg: string, ...supportingDetails: any[]): void {
@@ -22,5 +16,16 @@ export class LogUtil {
 
   static error(msg: string, ...supportingDetails: any[]): void {
     LogUtil.emitLog('error', msg, supportingDetails);
+  }
+
+  private static emitLog(logType: 'log' | 'info' | 'warn' | 'error', msg: string, supportingDetails: any[]) {
+
+    if (ConfigService.config.debug) {
+      if (supportingDetails.length > 0) {
+        console[logType](msg, supportingDetails);
+      } else {
+        console[logType](msg);
+      }
+    }
   }
 }
